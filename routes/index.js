2da70cde601b053a0b76ebfc069844c58d2d34a6
Ajaykumar Yadav <ajay.yadav@weboniselab.com>
2014-09-20 20:55:26 +0530

    mysql connect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    hostname: 'localhost',
+    user: 'tmg',
+    password: 'tmg',
+    database: 'node-tmg'
+});
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  connection.query('SELECT * FROM tmg',function(err,rows,fields)     {
+        console.log(rows);
+        console.log("-------------------------");
+        for (var i in rows) {
+            console.log('Data: ', rows[i].name);
+        }
+        console.log("-------------------------");
+        res.render('index', { title: 'Express',data:rows});
+    });
 });
 
 module.exports = router;
