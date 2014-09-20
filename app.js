bb45ec69d1e046db48b894d39a4307092f22c79f
harshal <harshal@harshal.(none)>
2014-09-20 20:43:32 +0530

    mysql module added

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+//var login = require('./routes/login');
 var users = require('./routes/users');
+
 
 var app = express();
 
@@ -19,7 +21,8 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.cookieParser('shhhh, very secret'));
+app.use(express.session());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    hostname: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'performance_management_db'
+})
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,6 +20,21 @@ router.get('/login', function(req, res) {
 //forgot password
 router.get('/forgot', function(req, res) {
     res.render('forgot', { title: 'Express' });
+});
+
+router.post('/login',function(req,res){
+ var userName=req.body.userName;
+ var password=req.body.password;
+connection.query('select * from users where username=admin and password=admin',function(error,row,fields){
+if(error){
+    console.log('error in query');
+}
+    console.log(fields);
+});
+    if(verified){
+        req.session.username = username;
+    }
+    res.send(test);
 });
 
 module.exports = router;
