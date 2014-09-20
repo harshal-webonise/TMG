8cb98f8c13a571238e811b9590b0c967651ad57c
Vicky Shinde <vicky.shinde@weboniselab.com>
2014-09-20 20:56:47 +0530

    Working on User KRA form

---
+++ public/js/custom.js
@@ -0,0 +1,27 @@
+var _customRadioButton = function(){
+    $("input[type='radio']").each(function(){
+        if(this.checked){
+            if(this.disabled)
+                $(this).wrap("<span class='radioWrap checked disabled'></span>");
+            else
+                $(this).wrap("<span class='radioWrap checked'></span>");
+        }
+        else{
+            if(this.disabled)
+                $(this).wrap("<span class='radioWrap disabled'></span>");
+            else
+                $(this).wrap("<span class='radioWrap'></span>");  
+        }
+        $(this).parent().after("<label for="+ $(this).attr("id")+">"+ $(this).attr("value") +"</label>")
+    });
+
+    $(document).on("change","input[type='radio']", function () {
+        $("input[name="+ $(this).attr("name") +"]").parent().removeClass("checked");
+        $(this).parent().addClass("checked");
+    });
+}
+
+
+$(window).load(function() {
+    _customRadioButton();   
+});
